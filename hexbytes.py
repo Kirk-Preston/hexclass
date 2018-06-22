f14922e3f3fa17ea3f1084c4932da9bd626a1dc7
Kirk Preston <kirk.preston@unipartdorman.co.uk>
2018-06-22 09:10:00 +0100

    thingy

---
+++ hexbytes.py
@@ -0,0 +1,24 @@
+class hexbytes():
+    def __init__(self,string):
+        self.string = string
+    def lilendian(self):
+        bytestring = []
+        for char in self.string:
+            hexchar = char.encode("hex")
+            hexesc = ('\\x')
+            hexconcat = (hexesc+hexchar)
+            bytestring.append(hexconcat)
+        return(''.join(bytestring))
+    def bigendian(self):
+        bytestring = []
+        for char in self.string:
+            hexchar = char.encode("hex")
+            hexesc = ('\\x')
+            hexconcat = (hexesc+hexchar)
+            bytestring.append(hexconcat)
+        return(''.join(reversed(bytestring)))
+'''   
+tobytes = hexbytes("test")
+print(tobytes.lilendian())
+print(tobytes.bigendian())
+'''
